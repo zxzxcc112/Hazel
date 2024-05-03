52b053119c3cabc9f4a0714eb6f94acb1a08fe51
PikaMax <pikazxzxcc112@gmail.com>
2024-05-04 04:47:25 +0900

    Single Shader 2D Renderer Use single shader to supports flat color and texture Add template 'CreateRef/Scope'

--- Hazel/src/Hazel/Renderer/Renderer2D.cpp
+++ Hazel/src/Hazel/Renderer/Renderer2D.cpp
@@ -13,8 +13,8 @@ namespace Hazel
 	struct Renderer2DStorage
 	{
 		Ref<VertexArray> QuadVertexArray;
-		Ref<Shader> FlatColorShader;
 		Ref<Shader> TextureShader;
+		Ref<Texture2D> WhiteTexture;
 	};
 
 	static Renderer2DStorage *s_Data;
@@ -50,7 +50,10 @@ namespace Hazel
 		squareindexBuffer = IndexBuffer::Create(squareindices, sizeof(squareindices) / sizeof(uint32_t));
 		s_Data->QuadVertexArray->SetIndexBuffer(squareindexBuffer);
 
-		s_Data->FlatColorShader = Shader::Create("assets/shaders/FlatColor.glsl");
+		s_Data->WhiteTexture = Texture2D::Create(1, 1);
+		uint32_t whiteTextureData = 0xffffffff;
+		s_Data->WhiteTexture->SetData(&whiteTextureData, sizeof(uint32_t));
+
 		s_Data->TextureShader = Shader::Create("assets/shaders/Texture.glsl");
 		s_Data->TextureShader->Bind();
 		s_Data->TextureShader->SetInt("u_Texture", 0);
@@ -63,9 +66,6 @@ namespace Hazel
 
 	void Renderer2D::BeginScene(const OrthographicCamera& camera)
 	{
-		s_Data->FlatColorShader->Bind();
-		s_Data->FlatColorShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
-
 		s_Data->TextureShader->Bind();
 		s_Data->TextureShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
 	}
@@ -82,12 +82,13 @@ namespace Hazel
 
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
 	{
-		s_Data->FlatColorShader->Bind();
-		s_Data->FlatColorShader->SetFloat4("u_FlatColor", color);
+		s_Data->TextureShader->SetFloat4("u_Color", color);
 
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * /*rotate*/
 							  glm::scale(glm::mat4(1.0f), { size, 1.0f });
-		s_Data->FlatColorShader->SetMat4("u_Transform", transform);
+		s_Data->TextureShader->SetMat4("u_Transform", transform);
+
+		s_Data->WhiteTexture->Bind();
 
 		s_Data->QuadVertexArray->Bind();
 		RenderCommand::DrawIndexed(s_Data->QuadVertexArray);
@@ -100,7 +101,7 @@ namespace Hazel
 
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture)
 	{
-		s_Data->TextureShader->Bind();
+		s_Data->TextureShader->SetFloat4("u_Color", glm::vec4(1.0f));
 
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * /*rotate*/
 			glm::scale(glm::mat4(1.0f), { size, 1.0f });
--- Hazel/src/Hazel/Renderer/Texture.h
+++ Hazel/src/Hazel/Renderer/Texture.h
@@ -14,6 +14,8 @@ namespace Hazel
 		virtual uint32_t GetWidth() const = 0;
 		virtual uint32_t GetHeight() const = 0;
 
+		virtual void SetData(void* data, uint32_t size) = 0;
+
 		virtual void Bind(uint32_t slot = 0) const = 0;
 	};
 
@@ -22,6 +24,7 @@ namespace Hazel
 	public:
 		virtual ~Texture2D() = default;
 
+		static Ref<Texture2D> Create(uint32_t width, uint32_t height);
 		static Ref<Texture2D> Create(const std::string& path);
 	};
 }
